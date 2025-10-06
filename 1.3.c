df5a1d9c9d737f5517c39fd5bd2f685b6fe05445
Прозоров Алексей Владимирович ТКИ-141 <127674446+mareh321@users.noreply.github.com>
2025-10-06 12:04:31 +0300

    Create 1.3.c

---
+++ 1.3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+/**
+* @brief Рассчитывает время по заданной формуле
+* param R - значение сопротивления
+* param I - значение силы тока
+* param Q - значение
+* return рассчитанная площадь
+*/
+double getA(double R, double I, double Q);
+/**
+* @brief Точка входа в программу
+* @return Возвращает 0, если программа выполнена корректно
+*/
+int main() {
+	setlocale(LC_ALL, "Russian");
+	double R, I, Q;
+	printf("Сопротивление -");
+	scanf_s("\n%lf", &R);
+	printf("Сила тока -");
+	scanf_s("\n%lf", &I);
+	printf("Количество теплоты -");
+	scanf_s("%lf", &Q);
+	printf("%.2lf",getA(R, I, Q));
+	return 0;
+}
+double getA(double R, double I, double Q){
+	return ((Q*I*I)/R);
+}
