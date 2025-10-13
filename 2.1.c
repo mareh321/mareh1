e8f6766e974093aa92eab115a7713f34daed299b
Прозоров Алексей Владимирович ТКИ-141 <127674446+mareh321@users.noreply.github.com>
2025-10-13 14:29:15 +0300

    Create 2.1.c

---
+++ 2.1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <math.h>
+/**
+* @brief Рассчитывает среднее арифметическое кубов этих чисел
+* param number1 - значение первого числа
+* param number2 - значение второго числа
+* return рассчитанное значение
+*/
+double getA(double number1, double number2);
+/**
+* @brief Рассчитывает геометрическое модулей этих чисел
+* param number1 - значение первого числа
+* param number2 - значение второго числа
+* return рассчитанное значение
+*/
+double getB(double number1, double number2);
+/**
+ * @brief считывает значение,
+ * введенное с клавиатуры с проверкой ввода
+ * @return считанное значение
+ */
+double getValue();
+
+int main() {
+    double const number1, number2;
+
+    printf("Введите первое число: ");
+    double number1 = getValue();
+
+
+    printf("Введите второе число: ");
+    double number2 = getValue();
+
+    printf("%lf\n", getA(number1, number2));
+    printf("%lf\n", getB(number1, number2));
+    return 0;
+}
+
+double getA(double number1, double number2) {
+    printf("Среднее арифметическое кубов этих чисел: ");
+    return (pow(number1, 3) + pow(number2, 3)) / 2.0;
+}
+double getB(double number1, double number2) {
+    printf("Среднее геометрическое модулей этих чисел: ");
+    return pow(fabs(number1 * number2), 0.5);
+}
+double getValue()
+{
+    double value = 0;
+    if (!scanf("%lf", &value))
+    {
+        printf("Error\n");
+        abort();
+    }
+    return value;
+}
