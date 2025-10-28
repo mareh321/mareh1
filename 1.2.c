55efc3dfce9c27721033c3fb55a375e3891039ff
Прозоров Алексей Владимирович ТКИ-141 <127674446+mareh321@users.noreply.github.com>
2025-10-28 09:33:13 +0300

    Update 1.2.c

--- 1.2.c
+++ 1.2.c
@@ -7,21 +7,21 @@
 * param x - значение второй стороны
 * return рассчитанная площадь
 */
-double getPloshad_treug(double st1, double st2);
+double getPloshad_treug(const double st1,const double st2);
 /**
 * @brief Рассчитывает площадь прямоугольника по заданой формуле
 * param x - значение первой стороны
 * param x - значение второй стороны
 * return рассчитанная площадь
 */
-double getPloshad_pryamoug(double st1, double st2);
+double getPloshad_pryamoug(const double st1,const double st2);
 /**
 * @brief Точка входа в программу
 * @return Возвращает 0, если программа выполнена корректно
 */
 int main() {
     setlocale(LC_ALL, "Russian");
-    double st1, st2;
+    double st1 = 0, st2 = 0;
 
     printf("Первая сторона - ");
     scanf_s("%lf", &st1);
