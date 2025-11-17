b6f7ff3a78ff631ec5e70da33fca9551e214990e
Прозоров Алексей Владимирович ТКИ-141 <127674446+mareh321@users.noreply.github.com>
2025-11-17 10:38:06 +0300

    Update 2.1.c

--- 2.1.c
+++ 2.1.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <locale.h>
 /**
 * @brief Рассчитывает среднее арифметическое кубов этих чисел
 * param number1 - значение первого числа
 * param number2 - значение второго числа
 * return рассчитанное значение
 */
-double getSrArifm(const double number1,const double number2);
+double getSrArifm(const double number1, const double number2);
 /**
 * @brief Рассчитывает геометрическое модулей этих чисел
 * param number1 - значение первого числа
 * param number2 - значение второго числа
 * return рассчитанное значение
 */
-double getSrGeom(const double number1,const double number2);
+double getSrGeom(const double number1, const double number2);
 /**
  * @brief считывает значение,
  * введенное с клавиатуры с проверкой ввода
@@ -25,8 +27,7 @@ double getValue();
 * @return Возвращает 0, если программа выполнена корректно
 */
 int main() {
-    double number1 = 0, number2 = 0;
-
+    setlocale(LC_ALL, "Russian");
     printf("Введите первое число: ");
     double number1 = getValue();
 
@@ -39,18 +40,18 @@ int main() {
     return 0;
 }
 
-double getSrArifm(const double number1,const double number2) {
+double getSrArifm(const double number1, const double number2) {
     printf("Среднее арифметическое кубов этих чисел: ");
-    return (pow(number1, 3) + pow(number2, 3)) / 2.0;
+    return ((pow(number1, 3) + pow(number2, 3)) / 2.0);
 }
-double getSrGeom(const double number1,const double number2) {
+double getSrGeom(const double number1, const double number2) {
     printf("Среднее геометрическое модулей этих чисел: ");
-    return pow(fabs(number1 * number2), 0.5);
+    return sqrt(fabs(number1) * fabs(number2));
 }
 double getValue()
 {
     double value = 0;
-    if (!scanf("%lf", &value))
+    if (!scanf_s("%lf", &value))
     {
         printf("Error\n");
         abort();
