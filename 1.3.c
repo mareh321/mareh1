c6baf1e13d006258ddc8b6d46cb463e7a2529efa
Прозоров Алексей Владимирович ТКИ-141 <127674446+mareh321@users.noreply.github.com>
2025-11-17 10:24:55 +0300

    Update 1.3.c

--- 1.3.c
+++ 1.3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <locale.h>
+
 /**
 * @brief Рассчитывает время по заданной формуле
 * param R - значение сопротивления
@@ -8,23 +9,23 @@
 * param Q - значение
 * return рассчитанная площадь
 */
-double getZakonOma(double R, double I, double Q);
+double getZakonOma(const double R,const double I,const double Q);
 /**
 * @brief Точка входа в программу
 * @return Возвращает 0, если программа выполнена корректно
 */
 int main() {
 	setlocale(LC_ALL, "Russian");
-	double R, I, Q;
+	double R = 0, I = 0, Q = 0;
 	printf("Сопротивление -");
 	scanf_s("\n%lf", &R);
 	printf("Сила тока -");
 	scanf_s("\n%lf", &I);
 	printf("Количество теплоты -");
 	scanf_s("%lf", &Q);
-	printf("%.2lf",getA(R, I, Q));
+	printf("%.2lf",getZakonOma(R, I, Q));
 	return 0;
 }
-double getZakonOma(double R, double I, double Q){
+double getZakonOma(const double R,const double I,const double Q){
 	return ((Q*I*I)/R);
 }
