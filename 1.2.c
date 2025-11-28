d2109ed8d432ddc0187cb6c915bd11f86ab1310d
Прозоров Алексей Владимирович ТКИ-141 <127674446+mareh321@users.noreply.github.com>
2025-11-28 14:12:00 +0300

    Update 1.2.c

--- 1.2.c
+++ 1.2.c
@@ -2,45 +2,59 @@
 #include <math.h>
 #include <locale.h>
 /**
+ * @brief считывает значение,
+ * введенное с клавиатуры с проверкой ввода
+ * @return считанное значение
+ */
+double getValue();
+/**
 * @brief Рассчитывает площадь треугольника по заданой формуле
-* param x - значение первой стороны 
+* param x - значение первой стороны
 * param x - значение второй стороны
 * return рассчитанная площадь
 */
-double getPloshad_treug(const double st1,const double st2);
+double getThe_area_of_the_triangle(const double st1, const double st2);
 /**
 * @brief Рассчитывает площадь прямоугольника по заданой формуле
 * param x - значение первой стороны
 * param x - значение второй стороны
 * return рассчитанная площадь
 */
-double getPloshad_pryamoug(const double st1,const double st2);
+double getRectangle_area(const double st1, const double st2);
 /**
 * @brief Точка входа в программу
 * @return Возвращает 0, если программа выполнена корректно
 */
 int main() {
     setlocale(LC_ALL, "Russian");
-    double st1 = 0, st2 = 0;
-
     printf("Первая сторона - ");
-    scanf_s("%lf", &st1);
-
+    double st1 = getValue();
     printf("Вторая сторона - ");
-    scanf_s("%lf", &st2);
-    getPloshad_treug(st1, st2);
-    getPloshad_pryamoug(st1, st2);
+    double st2 = getValue();
+    getThe_area_of_the_triangle(st1, st2);
+    getRectangle_area(st1, st2);
     return 0;
 }
 
-double getPloshad_treug(const double st1,const double st2) {
+double getThe_area_of_the_triangle(const double st1, const double st2) {
     printf("Площадь треугольника - ");
     printf("%.2lf", st1 * st2 * 0.5);
-    
+
 }
 
-double getPloshad_pryamoug(const double st1,const double st2) {
+double getRectangle_area(const double st1, const double st2) {
     printf("\nПлощадь прямоугольника - ");
     printf("%.2lf", st1 * st2);
-    
+
+}
+
+double getValue() {
+    double value = 0;
+    if (!scanf_s("%lf", &value)) {
+        printf("----");
+        abort();
+    }
+    else {
+        return value;
+    }
 }
