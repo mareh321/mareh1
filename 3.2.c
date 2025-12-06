434a8588a95bd6536180a65faaa883aee5789eb6
Прозоров Алексей Владимирович ТКИ-141 <127674446+mareh321@users.noreply.github.com>
2025-12-07 00:39:42 +0300

    Update 3.2.c

--- 3.2.c
+++ 3.2.c
@@ -1,56 +1,126 @@
 #include <stdio.h>
-#include <locale.h>
+#include <stdlib.h>
 #include <math.h>
+#include <locale.h>
 /**
- * @brief считывает значение,
- * введенное с клавиатуры с проверкой ввода
- * @return считанное значение
+ * @brief Считывает введённое с клавиатуры целое значение и проверяет на правильность ввода
+ * @return введённое значение
+ */
+int Value();
+
+/**
+ * @brief Считывает введённое с клавиатуры вещественное значение и проверяет на правильность ввода
+ * @return введённое значение
  */
 double getValue();
+
 /**
- * @brief рассчитывает сумму рекурсивной функции  по заданной формуле и переменной
- * @param k переменная
+ * @brief рассчитывает сумму n членов последовательности
+ * @param n - заданное число членов
  * @return сумма
  */
-double getRekurs(const double k);
-/**
- * @brief рассчитывает фактариал числа
- * @param r число
- * @return фактариал числа
- */
-double getFact(const double r);
-/**
-* @brief точка входа в программу
-* @return возврат 0 в случае если программа работает правиьлно
- */
-int main() {
-	setlocale(LC_ALL, "Russian");
-	printf("Введите переменную n - ");
-	double n = getValue();
-	printf("%lf", getRekurs(n));
-	return 0;
+double SumN(const int n);
 
-}
-double getValue() {
-	double value = 0;
-	if (!scanf_s("%lf", &value)) {
-		printf("----");
-		abort();
-	}
-	else {
-		return value;
-	}
-}
-double getFact(const double r) {
-	if (r == 1.0) {
-		return 1;
-	}
-	return r * getFact(r - 1);
+/**
+ * @brief рассчитывает сумму членов последовательности, с учетом e
+ * @param e - заданное число
+ * @return сумма
+ */
+double SumE(const double e);
+
+/**
+ * @brief рассчитывает коэффициент рекуррентного выражения
+ * @param i текущий индекс
+ * @return рассчитанное значение коэффициента
+ */
+double Recurent(const int i);
+
+/**
+ * @brief проверяет, что число положительное
+ * @param value - проверяемое значение
+ */
+void checkPositive(const double i);
+
+/**
+ * @brief Точка входа в программу
+ * @return 0, если программа выполнена корректно, иначе 1
+ */
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    printf("Введите n:");
+    int n = Value();
+    checkPositive(n);
+    printf("Сумма %d чисел последовательности равна %lf\n", n, SumN(n));
+    printf("*Вместо '.' используйте ','\nВведите e:");
+    double e = getValue();
+    checkPositive(e);
+    printf("Сумма последовательности с учетом числа e равна %lf\n",  SumE(e));
+
+    return 0;
 }
 
-double getRekurs(const double k) {
-	if (k == 0) {
-		return 0;
-	}
-	return ((pow(-1, k) * k) / getFact(k)) + getRekurs(k - 1);
+int Value()
+{
+    int value = 0;
+    if (!scanf_s("%d", &value))
+    {
+        printf("Error\n");
+        exit(1);
+    }
+    return value;
 }
+
+double getValue()
+{
+    double value = 0;
+    if (!scanf_s("%lf", &value))
+    {
+        printf("Error\n");
+        exit(1);
+    }
+    return value;
+}
+
+double SumN(const int n)
+{
+    if (n == 0)
+        return 0.0;
+
+    double current = -1.0;
+    double result = current;
+    for (int i = 1; i <= n; ++i)
+    {
+        current *= Recurent(i);
+        result += current;
+    }
+
+    return result;
+}
+double SumE(const double e)
+{
+    double current = -1.0;
+    double result = 0;
+    for (int i = 1; fabs(current) > e; i++)
+    {
+        result += current;
+        current *= Recurent(i);
+    }
+    return result;
+}
+
+double Recurent(const int i)
+{
+
+    return -1.0 / i;
+}
+
+void checkPositive(const double i)
+{
+    if (i <= 0)
+    {
+        printf("Ошибка, число должно быть положительным!\n");
+        exit(1);
+    }
+}
+
